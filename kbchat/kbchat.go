89516ad5a510d092f92f56e545740217b2e2bbcd
malware-unicorn <amanda@secured.org>
2020-03-09 21:24:18 -0700

    updaing oneshot

--- kbchat/kbchat.go
+++ kbchat/kbchat.go
@@ -89,7 +89,7 @@ func (a *API) auth() (string, error) {
 		return username, nil
 	}
 	if a.runOpts.Oneshot == nil {
-	    a.runOpts.Oneshot = &OneShot{
+	    a.runOpts.Oneshot = &OneShotOptions{
 		    Username: os.Getenv("KEYBASE_USERNAME"),
 		    Paperkey: os.Getenv("KEYBASE_PAPERKEY")
 	    }
